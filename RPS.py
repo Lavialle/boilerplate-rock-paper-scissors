ee68370cfd062a247662e3806959c7d3caadc551
Ange Lavialle <ange.lavialle@student-cs.fr>
2024-11-07 15:29:18 +0000

    First try

--- RPS.py
+++ RPS.py
@@ -1,10 +1,18 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
+from collections import deque, Counter
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
+    history_size = 20
+    window = deque(maxlen=history_size)
+    window.append(prev_play)
+    counts = Counter(window)
+    total = len(window)
+    proba = {'R':counts['R'] / total if total > 0 else 0,'P':counts['P'] / total if total > 0 else 0,'S':counts['S'] / total if total > 0 else 0}
+    max_proba = max(proba, key = proba.get)
+    if max_proba == 'R':
+        guess = 'P'
+    elif max_proba == 'P':
+        guess = 'S'
+    else:
+        guess = 'R'
     return guess
